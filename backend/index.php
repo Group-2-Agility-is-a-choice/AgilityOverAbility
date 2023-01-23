111fe69d6c858a7196de18ba8c1a48f4caa45202
Adam Mathieson <adam@genav.ch>
2023-01-23 16:14:01 +0000

    + base API structure

--- backend/index.php
+++ backend/index.php
@@ -1,1 +1,9 @@
 <?php
+$_routes = glob("./routes/*.php");
+foreach ($_GET as $key => $value) {
+    $index = array_search("./routes/" . $key . ".php", $_routes);
+    if ($index !== false) {
+        include $_routes[$index];
+        callback();
+    }
+}
