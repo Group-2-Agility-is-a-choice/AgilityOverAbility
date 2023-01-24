205430a6551a49db614b81e1e71e7a81a4754c9a
lujamitch@gmail.com <lujamitch@gmail.com>
2023-01-24 15:25:07 +0000

    Added ingredients and details about them

--- backend/routes/getFullRecipe.php
+++ backend/routes/getFullRecipe.php
@@ -6,9 +6,22 @@ function callback()
     http_response_code(200);
     $pdo = PDO_config("LGLAdmin", "URlY6qOhsbrXqHr9");
 
+    $stmt = $pdo->prepare("SELECT Ingredient.Name, RecipeIngredients.Quantity, RecipeIngredients.Unit
+    FROM Recipe 
+    JOIN RecipeIngredients ON (Recipe.RecipeID = RecipeIngredients.RecipeID) 
+    JOIN Ingredient ON (RecipeIngredients.IngredientID = Ingredient.IngredientID)
+    WHERE Recipe.RecipeID = ?");
+    $stmt->execute([$_GET['RecipeID']]);
+    $ingredients = $stmt->fetchAll();
+
     $stmt = $pdo->prepare("SELECT * FROM `Recipe` WHERE `RecipeID` = ?");
     $stmt->execute([$_GET['RecipeID']]);
-    $rtn = $stmt->fetchAll();
+    $recipeDetails = $stmt->fetchAll();
+
+    $rtn = [
+        "ingredients" => $ingredients,
+        "recipeDetails" => $recipeDetails
+    ];
 
     return [
         "content-type" => "application/json",
