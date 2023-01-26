b0261f6fe082d4acffeee0bacea18325e449432d
NiamhGedge <nfgedge@icloud.com>
2023-01-26 16:57:11 +0000

    spice level

--- Questionaire.js
+++ Questionaire.js
@@ -18,12 +18,14 @@ function passAllerg(){
     let els = document.getElementsByClassName("allerg-list");
     for(let i = 0; i < els.length; i++){
         if (els[i].checked == false){
+            // PASS IN NAME????
             allergIngredients += "&ingredientsID[]=" + els[i].id;
         }
     };
 }
 
 
+// HTML FUNCTINO THING NEEDED
 
 
 // WHICH INGREDIENTS DO YOU HAVE LIST
@@ -32,48 +34,42 @@ function listOwnedIngredients(){
         rtn.json().then((data)=>{
             let ownedIngr = "<ul style = 'list-style:none;'>";
             data.foreach((item)=>{
-                listOfIngrs += `<lu> <input oneclick = FUNCNTION' class = "owned-list" type = "checkbox" value = "${item.IngredientID}" id = ${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
-            })
+                listOfIngrs += `<lu> <input oneclick = passOwnedIngr()' class = "owned-list" type = "checkbox" value = "${item.IngredientID}" id = ${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
+            });
+            listOfIngrs += `</ul>`;
+            document.getElementById(BACKEND NAME ).innerHTML = listOfIngrs;
         })
 
     })
 }
 
+// INGRESDIENTS OWNED TO BACKEND
+function passOwnedIngr(){
+    let ownedIngr = "";
+    let els = document.getElementsByClassName("owned-list");
+    for(let i = 0; i < els.length; i++){
+        if(els[i].checked == true){
+            // PASS IN NAME?????
+            ownedIngr += "&ingredientsID[]" + els[i].id;
+        }
+    };
+}
 
 
+// HTML FUNCTINO THING NEEDED
 
 
+// DO YOU LIKE SPICY FOOD CHECK 
+function listSpice(){
+    '<input type = "range" id = "Spice" label = "Spice" min = "0" max = "3">'
+    '<label for = "Spice" >Spice</label>'
+    let spiceLevel = document.getElementById("Spice");
+    spiceLevel += "name?????";
 
 
-// function allIngredients(){
-//     fetch("backend?getIngredients").then((rtn)=>{
-//         rtn.json().then((data)=>{
-//             let ingredientsList = "<ul style='list-style:none;'>"
-//             data.forEach((item)=>{
-//                 ingredientsList += `<li> <input onclick='displayRecipes()' class="check-ingredients" type = "checkbox" value="${item.IngredientID}" id = "${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
-//             }); 
-//             ingredientsList += `</ul>`;  
-//             document.getElementById("getIngredients").innerHTML = ingredientsList; 
-//             displayRecipes();
-//         })
-//     });
-// }
+    
 
 
+}
 
 
-
-
-
-
-
-// function displayRecipes(){
-//     let relevantIngredients = "";
-
-//     let els = document.getElementsByClassName("check-ingredients");
-
-//     for(let i = 0; i < els.length ; i++){
-//         if (els[i].checked == false){
-//             relevantIngredients += "&ingredientsID[]=" + els[i].id;
-//         }
-//     };
