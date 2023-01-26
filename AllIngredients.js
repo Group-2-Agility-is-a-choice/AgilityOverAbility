9fdc9ccdd685fb336cbf075fa97f8a55ddf0f188
nicolejackson2 <2415277@dundee.ac.uk>
2023-01-26 12:18:05 +0000

    connected js to html and includes servings

--- AllIngredients.js
+++ AllIngredients.js
@@ -3,28 +3,36 @@ let ingredients = [];
 function allIngredients(){
     fetch("backend?getIngredients").then((rtn)=>{
         rtn.json().then((data)=>{
-            let ingredientsList = "<ul>";
-            data.getIngredients.foreach((item)=>{
-                ingredientsList += `<li> <input class="check-ingredients" type = "checkbox" value="${item.IngredientId}" id = "${item.IngredientId}"> <label for = "${item.IngredientId}"> ${item.Name}</label><br></li>`;
+            let ingredientsList = "<ul style='list-style:none;'>"
+            data.forEach((item)=>{
+                ingredientsList += `<li> <input onclick='displayRecipes()' class="check-ingredients" type = "checkbox" value="${item.IngredientID}" id = "${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
             }); 
             ingredientsList += `</ul>`;  
             document.getElementById("getIngredients").innerHTML = ingredientsList; 
+            displayRecipes();
         })
     });
 }
 
 function displayRecipes(){
     let relevantIngredients = "";
-    document.getElementsByClassName("check-ingredients").foreach((item)=>{
-        if (item.checked == false){
-            relevantIngredients += "&ingredientsID[]=" + item.id;
+
+    let els = document.getElementsByClassName("check-ingredients");
+
+    for(let i = 0; i < els.length ; i++){
+        if (els[i].checked == false){
+            relevantIngredients += "&ingredientsID[]=" + els[i].id;
         }
-    });
+    };
+
+
+
+
     // BACKEND NEEDS IMPLEMENTED SO RELEVANT PARAMETERS CAN BE PASSED IN
     fetch("backend?searchRecipes").then((rtn)=>{
         rtn.json().then((data)=>{
             let recipeList = "<ul>";
-            data.seachRecipes.foreach((item)=>{
+            data?.seachRecipes.forEach((item)=>{
                 recipeList += `<li>${item.Name} ${item.Image} ${item.SpiceLevel} ${item.ServingAmount}</li>`;
             }); 
             recipeList += `</ul>`;  
