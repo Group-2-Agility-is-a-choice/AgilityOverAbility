5fcddec0339032f463b1f2f06b4051d721b56ec2
lujamitch@gmail.com <lujamitch@gmail.com>
2023-01-26 13:57:30 +0000

    endpoint done and tested

--- backend/routes/searchRecipes.php
+++ backend/routes/searchRecipes.php
@@ -6,8 +6,6 @@ function callback()
     http_response_code(200);
     $pdo = PDO_config("LGLAdmin", "URlY6qOhsbrXqHr9");
 
-    //$ingredients = [$_GET['ingredientsID']];
-
 
     if (!isset($_GET['ingredientsID'])) {
 
@@ -20,45 +18,42 @@ function callback()
     } else {
 
         $rslt = [];
+        $temp = [];
         $size = sizeof($_GET['ingredientsID']);
 
         for ($x = 0; $x < $size; $x++) {
 
 
-            $stmt = $pdo->prepare("SELECT Recipe.RecipeID FROM Recipe JOIN RecipeIngredients ON (Recipe.RecipeID = RecipeIngredients.RecipeID) JOIN Ingredient ON (RecipeIngredients.IngredientID = Ingredient.IngredientID) WHERE Ingredient.IngredientID = ?");
+            $stmt = $pdo->prepare("SELECT Recipe.Name, Recipe.Image, Recipe.ServingAmount, Recipe.Spicelevel FROM Recipe JOIN RecipeIngredients ON (Recipe.RecipeID = RecipeIngredients.RecipeID) JOIN Ingredient ON (RecipeIngredients.IngredientID = Ingredient.IngredientID) WHERE Ingredient.IngredientID = ?");
             $stmt->execute([$_GET['ingredientsID'][$x]]);
             $rtn = $stmt->fetchAll();
 
             if ($x === 0) {
                 $rslt = $rtn;
-                //print_r($rslt);
 
             } else {
-               /*  foreach ($rslt as $res) {
-                    echo "ID: " . $res['RecipeID'] . "\n";
 
-                    if (!in_array($res, $rtn)) {
+                $temp = $rslt;
+                $rslt_size = (sizeof($rslt));
 
-                        unset($res);
-                    }
-                } */
-                    $rslt_size = (sizeof($rslt));
                 for ($y = 0; $y < $rslt_size; $y++) {
-                    echo "ID: " . $rslt[$y]['RecipeID'] . "\n";
 
-                    if (!in_array($rslt[$y], $rtn)) {
+                    if (!in_array($temp[$y], $rtn)) {
 
-                        unset($rslt[$y]);
+                        unset($temp[$y]); 
+
                     }
-
+                
 
 
                 }
-            }
-            print_r(sizeof($rslt));
-            //print_r($rslt);
+                $temp = array_values($temp);
+                $rslt = $temp;
 
+            }
         }
+
+        $rtn = $rslt;
 
     }
 
@@ -66,7 +61,7 @@ function callback()
 
     return [
         "content-type" => "application/json",
-        "content" => json_encode($rslt, JSON_PRETTY_PRINT)
+        "content" => json_encode($rtn, JSON_PRETTY_PRINT)
     ];
 
 
