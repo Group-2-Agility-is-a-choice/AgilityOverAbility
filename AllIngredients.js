4281163327ce657de9f5dc2d6ec912eb0c65b4c3
nicolejackson2 <2415277@dundee.ac.uk>
2023-01-26 14:27:53 +0000

    displays recipes in cards

--- AllIngredients.js
+++ AllIngredients.js
@@ -31,11 +31,21 @@ function displayRecipes(){
     // BACKEND NEEDS IMPLEMENTED SO RELEVANT PARAMETERS CAN BE PASSED IN
     fetch("backend?searchRecipes").then((rtn)=>{
         rtn.json().then((data)=>{
-            let recipeList = "<ul>";
-            data?.seachRecipes.forEach((item)=>{
-                recipeList += `<li>${item.Name} ${item.Image} ${item.SpiceLevel} ${item.ServingAmount}</li>`;
-            }); 
-            recipeList += `</ul>`;  
+            let recipeList = "";
+            data?.forEach((item)=>{
+                recipeList += `<div class="card mb-3 border border-success" style="max-width: 800px;" ><div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="${item.Image}" style="height:100%;width:100%;" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h4 class="card-title"><a href="recipe.html">${item.Name}</h4></a>
+                    <p class="card-text mt-4"><small class="text-muted">Spice: ${item.Spicelevel}</small></p>
+                    <p class="card-text"><small class="text-muted">Serves: ${item.ServingAmount}</small></p>
+                  </div>
+                </div>
+              </div></div>`;
+            });   
             document.getElementById("seachRecipes").innerHTML = recipeList; 
         })
     });
