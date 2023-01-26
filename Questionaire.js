308e73d3ae212591e881d3618e0dfe2432e0fc95
NiamhGedge <94126649+NiamhGedge@users.noreply.github.com>
2023-01-26 17:58:07 +0000

    Update Questionaire.js

--- Questionaire.js
+++ Questionaire.js
@@ -1,75 +1,77 @@
+// WHICH INGREDIENTS DO YOU HAVE LIST
+function listOwnedIngredients(){
+    fetch(backend?getIngredients).then((rtn)=>{
+        rtn.json().then((data)=>{
+            let ownedIngr = "<ul style = 'list-style:none;'>";
+            data.foreach((item)=>{
+                listOfIngrs += `<lu> <input class = "owned-list" type = "checkbox" value = "${item.IngredientID}" id = ${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
+            });
+            listOfIngrs += `</ul>`;
+            document.getElementById("question-have").innerHTML = listOfIngrs;
+        })
+
+    })
+}
+
+
 // LIST OF POTENTIAL ALLERGENS WITH CHECKBOXES
-function listAllerg(){
-    fetch("backend? WHATEVRR NAME IS").then((rtn)=>{
+function listAvoid(){
+    fetch("backend?getIngredients").then((rtn)=>{
         rtn.json().then((data)=>{
             let listOfIngr = "<ul style = 'list-style:none;'>";
             data.foreach((item)=>{
-                listOfIngr += `<li> <input onclick = 'passAllerg()' class = "allerg-list" type = "checkbox" value = "${item.IngredientID}" id = "${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
+                listOfIngr += `<li> <input class = "avoid-list" type = "checkbox" value = "${item.IngredientID}" id = "${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
             });
             listOfIngr += `</ul>`;
-            document.getElementById("BACKEND NAEM").innerHTML = listOfIngr;
+            document.getElementById("question-avoid").innerHTML = listOfIngr;
         })
     });
 }
 
-// ALLERGENS PASSED TO BACKEND
-function passAllerg(){
+
+// MAKE INTO ONE BIG PASS FUNCTION
+
+function passGoods(){
     let allergIngredients = "";
     let els = document.getElementsByClassName("allerg-list");
     for(let i = 0; i < els.length; i++){
         if (els[i].checked == false){
-            // PASS IN NAME????
+        // PASS IN NAME????
             allergIngredients += "&ingredientsID[]=" + els[i].id;
         }
     };
 }
 
 
-// HTML FUNCTINO THING NEEDED
 
 
-// WHICH INGREDIENTS DO YOU HAVE LIST
-function listOwnedIngredients(){
-    fetch(BACKEND NAME).then((rtn)=>{
-        rtn.json().then((data)=>{
-            let ownedIngr = "<ul style = 'list-style:none;'>";
-            data.foreach((item)=>{
-                listOfIngrs += `<lu> <input oneclick = passOwnedIngr()' class = "owned-list" type = "checkbox" value = "${item.IngredientID}" id = ${item.IngredientID}"> <label for = "${item.IngredientID}"> ${item.Name}</label><br></li>`;
-            });
-            listOfIngrs += `</ul>`;
-            document.getElementById(BACKEND NAME ).innerHTML = listOfIngrs;
-        })
-
-    })
-}
-
-// INGRESDIENTS OWNED TO BACKEND
-function passOwnedIngr(){
-    let ownedIngr = "";
-    let els = document.getElementsByClassName("owned-list");
-    for(let i = 0; i < els.length; i++){
-        if(els[i].checked == true){
-            // PASS IN NAME?????
-            ownedIngr += "&ingredientsID[]" + els[i].id;
-        }
-    };
-}
 
 
-// HTML FUNCTINO THING NEEDED
+
+// // ALLERGENS PASSED TO BACKEND
+// function passAllerg(){
+//     let allergIngredients = "";
+//     let els = document.getElementsByClassName("allerg-list");
+//     for(let i = 0; i < els.length; i++){
+//         if (els[i].checked == false){
+//             // PASS IN NAME????
+//             allergIngredients += "&ingredientsID[]=" + els[i].id;
+//         }
+//     };
+// }
 
 
-// DO YOU LIKE SPICY FOOD CHECK 
-function listSpice(){
-    '<input type = "range" id = "Spice" label = "Spice" min = "0" max = "3">'
-    '<label for = "Spice" >Spice</label>'
-    let spiceLevel = document.getElementById("Spice");
-    spiceLevel += "name?????";
+// // INGRESDIENTS OWNED TO BACKEND
+// function passOwnedIngr(){
+//     let ownedIngr = "";
+//     let els = document.getElementsByClassName("owned-list");
+//     for(let i = 0; i < els.length; i++){
+//         if(els[i].checked == true){
+//             // PASS IN NAME?????
+//             ownedIngr += "&ingredientsID[]" + els[i].id;
+//         }
+//     };
+// }
 
-
-    
-
-
-}
 
 
