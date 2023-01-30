0b3b4dd645d0419d55147676d484d1dac6526bba
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2023-01-30 15:17:27 +0000

    remove items working

--- Shopping.js
+++ Shopping.js
@@ -26,8 +26,8 @@ function load() {
         let amount = obj[key][1];
         let unit = obj[key][2];
         let item = obj[key][0];
-        let list = "<div class='form-check m-3' onclick='checkedBox(this)'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
-        let form = list+"<input type='number' id='"+item+"Input' min='0' style='width:75px;' value='"+amount+"' placeholder='"+amount+"'>";
+        let list = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
+        let form = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"EditBox'><label class='form-check-label' for='"+item+"EditBox'><input type='number' id='"+item+"Input' min='0' style='width:75px;' value='"+amount+"' placeholder='"+amount+"'>";
         list += amount+unit+" "+item; //how it displays: 5x bananas
         form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable
         list += "</label></div>";
@@ -41,12 +41,14 @@ function load() {
         //document.getElementById("save").classList.remove("hide");
         document.getElementById("edit").classList.remove("hide");
         document.getElementById("remove").classList.remove("hide");
+        document.getElementById("empty").classList.remove("hide");
     } catch {
       let empty = "<p>Your shopping cart is empty.</p>"//or error
       document.getElementById("shopping").innerHTML = empty;
       document.getElementById("save").classList.add("hide");
       document.getElementById("edit").classList.add("hide");
       document.getElementById("remove").classList.add("hide");
+      document.getElementById("empty").classList.add("hide");
     }
   } else {
     let empty = "<p>Your shopping cart is empty.</p>"
@@ -54,6 +56,7 @@ function load() {
     document.getElementById("save").classList.add("hide");
     document.getElementById("edit").classList.add("hide");
     document.getElementById("remove").classList.add("hide");
+    document.getElementById("empty").classList.add("hide");
     //document.getElementById("form").innerHTML = empty;
   }
 }
@@ -89,8 +92,8 @@ function update() {
         let itemID = item+"Input";
         let newAmount = document.getElementById(itemID).value;
         if (newAmount!=0) {
-          let list = "<div class='form-check m-3' onclick='checkedBox(this)'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
-          let form = list+"<input type='number' id='"+item+"Input' style='width:75px;' min='0' value='"+newAmount+"' placeholder='"+newAmount+"'>";
+          let list = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
+          let form = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"EditBox'><label class='form-check-label' for='"+item+"EditBox'><input type='number' id='"+item+"Input' style='width:75px;' min='0' value='"+newAmount+"' placeholder='"+newAmount+"'>";
           list += newAmount+unit+" "+item; //how it displays: 5x bananas
           form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable
           list += "</label></div>";
@@ -100,7 +103,8 @@ function update() {
           number += 1;
           shoppingList+=`"`+number+`":["${item}",${newAmount},"${unit}"],`;
         }else {
-          //delete from list somehow
+          //delete from list
+          //CHECK IF LIST IS EMPTY!!
         }
       });
         shoppingList = shoppingList.slice(0, -1); //removes comma
@@ -112,4 +116,76 @@ function update() {
     } catch {
       //error
     }
+}
+
+function remove() {
+  let storage = localStorage.getItem('storage'); //{1:['bananas',5,'x'],2:['flour',100,'mg']}
+  //alert(storage);
+  console.log(storage);
+
+  try {
+      const obj = JSON.parse(storage);
+
+      //console.log(obj['1'][1]+obj['1'][2]+" "+obj['1'][0]);
+      //5x bananas
+      let newList = "";
+      let newForm = "";
+
+      const keys = Object.keys(obj);
+      // print all keys
+      console.log(keys); //1:[banana,5,x]
+      // iterate over object
+      var shoppingList = "{";
+      let number = 0;
+
+      keys.forEach((key) => {
+
+        let amount = obj[key][1];
+        let unit = obj[key][2];
+        let item = obj[key][0];
+
+        let itemID = item+"Box";
+        let itemID2 = item+"EditBox";
+        let check = false;
+        if (document.getElementById(itemID).checked||document.getElementById(itemID2).checked) {
+          check = true;
+        }
+        console.log(itemID+" "+itemID2+" "+check);
+
+        if (check!=true) {
+          let list = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
+          let form = "<div class='form-check m-3'><input class='form-check-input' type='checkbox' id='"+item+"EditBox'><label class='form-check-label' for='"+item+"EditBox'><input type='number' id='"+item+"Input' style='width:75px;' min='0' value='"+amount+"' placeholder='"+amount+"'>";
+          list += amount+unit+" "+item; //how it displays: 5x bananas
+          form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable
+          list += "</label></div>";
+
+          newList += list;
+          newForm += form;
+          number += 1;
+          shoppingList+=`"`+number+`":["${item}",${amount},"${unit}"],`;
+        }else {
+          //delete from list
+          //CHECK IF LIST IS EMPTY!
+        }
+      });
+        shoppingList = shoppingList.slice(0, -1); //removes comma
+        shoppingList +=`}`;
+
+        document.getElementById("shopping").innerHTML = newList;
+        document.getElementById("form").innerHTML = newForm;
+        localStorage.setItem('storage',shoppingList); //new list is saved
+    } catch {
+      //error
+    }
+}
+
+function empty() {
+  localStorage.clear();
+  let empty = "<p>Your shopping cart is empty.</p>"//or error
+  document.getElementById("shopping").innerHTML = empty;
+  document.getElementById("form").innerHTML = empty;
+  document.getElementById("save").classList.add("hide");
+  document.getElementById("edit").classList.add("hide");
+  document.getElementById("remove").classList.add("hide");
+  document.getElementById("empty").classList.add("hide");
 }
