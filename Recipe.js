2a701ef78f1822905511eab99aaa95d30c75acd7
Adam Mathieson <adam@genav.ch>
2023-01-30 18:44:42 +0000

    + Patch recipe page - fixes issues caused by basket implementation

--- Recipe.js
+++ Recipe.js
@@ -1,9 +1,8 @@
-var shoppingList = "";
-var addList = "";
+let shoppingList = "";
 
 function getRecipe(id) {
-    fetch("backend/?getFullRecipe&RecipeID=" + id).then((rtn)=>{
-        rtn.json().then((data)=>{
+    fetch("backend/?getFullRecipe&RecipeID=" + id).then((rtn) => {
+        rtn.json().then((data) => {
             // display the recipe name
             document.getElementById("title").innerHTML = data.recipeDetails[0].Name;
             // display spice level
@@ -15,16 +14,16 @@ function getRecipe(id) {
             // display image
             document.getElementById("img").src = data.recipeDetails[0].Image;
             let ingredientsList = "<ul>";
-            shoppingList = `{`//`{"1":["bananas",5,"x"],"2":["flour",100,"mg"]}`;
+            shoppingList = `{`;
             // display ingredients
             let number = 0;
-            data.ingredients.forEach((item)=>{
+            data.ingredients.forEach((item) => {
                 number += 1;
                 ingredientsList += `<li>${item.Quantity} ${item.Unit} - ${item.Name}</li>`;
-                shoppingList+=`"`+number+`":["${item.Name}",${item.Quantity},"${item.Unit}"],`;
+                shoppingList += `"` + number + `":["${item.Name}",${item.Quantity},"${item.Unit}"],`;
             });
             shoppingList = shoppingList.slice(0, -1);
-            shoppingList +=`}`; //need to save this and to be returned by getList()
+            shoppingList += `}`; //need to save this and to be returned by getList()
             ingredientsList += "</ul>";
             document.getElementById("ingredients").innerHTML = ingredientsList;
         })
@@ -33,56 +32,44 @@ function getRecipe(id) {
 }
 
 function addList(id) {
-  fetch("backend/?getFullRecipe&RecipeID=" + id).then((rtn)=>{
-      rtn.json().then((data)=>{
-      if(localStorage.length!=0) {
-        let storage = localStorage.getItem('storage');
-        console.log(storage);
+    fetch("backend/?getFullRecipe&RecipeID=" + id).then((rtn) => {
+        rtn.json().then((data) => {
+            if (localStorage.length !== 0) {
+                let storage = localStorage.getItem('storage');
+                try {
+                    const obj = JSON.parse(storage);
+                    const keys = Object.keys(obj);
+                    // print all keys
+                    let tempList = "{";
+                    //skimmed items from list into order
+                    let amount = obj[key][1];
+                    let unit = obj[key][2];
+                    let itemName = obj[key][0];
 
-        try {
-          const obj = JSON.parse(storage);
-          const keys = Object.keys(obj);
-          // print all keys
-          let tempList = "{";
-          console.log(keys); //0:1,1:2
-          // iterate over object
-          keys.forEach((key) => {
-            //console.log(`${key}: ${obj[0][0]}`);
-          //});
-          //skimmed items from list into order
-            let amount = obj[key][1];
-            let unit = obj[key][2];
-            let itemName = obj[key][0];
+                    data.ingredients.forEach((item) => {
+                        if (itemName === item.Name) {
+                            let tempAmount = amount + item.Quantity; //will only work if same unit
+                            tempList += `"` + key + `":["${itemName}",${item.Quantity},"${tempAmount}"],`;
+                        } else {
+                            tempList += `"` + key + `":["${itemName}",${amount},"${unit}"],`;
+                        }
+                    })//gone through all items in basket, now add extra ingredients
+                    data.ingredients.forEach((item) => {
+                        keys.forEach((key) => {
+                            itemName = obj[key][0];
+                            if (itemName !== item.Name) { //if new ingredient not in list add it
+                                tempList += `"` + key + `":["${itemName}",${item.Quantity},"${item.Amount}"],`;
+                            }
+                        })
+                    })
+                    tempList = tempList.slice(0, -1);
+                    tempList += `}`;
 
-            data.ingredients.forEach((item))=>{
-              if (itemName==item.Name) {
-                let tempAmount = amount+item.Quantity; //will only work if same unit
-                tempList+=`"`+key+`":["${itemName}",${item.Quantity},"${tempAmount}"],`;
-              }else {
-                tempList+=`"`+key+`":["${itemName}",${amount},"${unit}"],`;
-              }
+                } catch {//error somewhere;
+                }
+            } else {
+                localStorage.setItem('storage', shoppingList); //list is empty so add just this
             }
-          })//gone through all items in basket, now add extra ingredients
-          data.ingredients.forEach((item))=>{
-            keys.forEach((key) => {
-              itemName = obj[key][0];
-              if (itemName!=item.Name) { //if new ingredient not in list add it
-                tempList+=`"`+key+`":["${itemName}",${item.Quantity},"${item.Amount}"],`;
-              }
-            })
-          })
-          tempList = tempList.slice(0, -1);
-          tempList +=`}`;
-
-        } catch {//error somewhere;
-        }
-      }else {
-        localStorage.setItem('storage',getList()); //list is empty so add just this
-      }
-    })
-  });
-}
-
-function getList() {
-    return shoppingList;
+        })
+    });
 }
