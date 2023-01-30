05b049d512b2860147fbc05e688864d0c3c1b130
nicolejackson2 <2415277@dundee.ac.uk>
2023-01-30 16:22:38 +0000

    added modal

--- Admin.js
+++ Admin.js
@@ -3,10 +3,10 @@ function showAllRecipes(){
         rtn.json().then((data)=>{
             let adminRecipeList = "<table style='list-style:none;'><tr><th>Title</th><th>Edit</th><th>Delete</th></tr>";
             data.seachRecipes.foreach((item)=>{
-                adminRecipeList += `<tr><td>${item.Name}</td><td><button onclick='editbutton()'  class = "btn btn-primary" id = "${item.RecipeID}"> Edit </button></td><td><button onclick='deleteButton()' class = "btn btn-primary" id = "${item.RecipeID}"> Delete</button></td> </tr>`;
+                adminRecipeList += `<tr><td>${item.Name}</td><td><button onclick='editbutton()'  class = "btn btn-primary" id = "${item.RecipeID}"> Edit </button> </td><td><button onclick='deleteButton()' class = "btn btn-primary" id = "${item.RecipeID}"> Delete</button></td> </tr>`;
             }); 
             adminRecipeList += `</table>`;  
-            document.getElementById("seachRecipes").innerHTML = recipeList; 
+            document.getElementById("editRecipes").innerHTML = recipeList; 
         })
 
     });
