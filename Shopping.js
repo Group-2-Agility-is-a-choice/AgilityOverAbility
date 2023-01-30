f77ef069800b21da98a93c781c3a8a64a14c605a
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2023-01-30 13:50:51 +0000

    edit item

--- Shopping.js
+++ Shopping.js
@@ -17,7 +17,7 @@ function load() {
 
       const keys = Object.keys(obj);
       // print all keys
-      console.log(keys); //0:1,1:2
+      console.log(keys); //1:[banana,5,x]
       // iterate over object
       keys.forEach((key) => {
         //console.log(`${key}: ${obj[0][0]}`);
@@ -26,10 +26,10 @@ function load() {
         let amount = obj[key][1];
         let unit = obj[key][2];
         let item = obj[key][0];
-        let list = "<div class='form-check m-3' onclick='checkedBox(this)'><input class='form-check-input' type='checkbox' id='ingredient1'><label class='form-check-label' for='ingredient1'>"
-        let form = list+"<input type='number' style='width:75px;' value='"+amount+"' placeholder='"+amount+"'>";
+        let list = "<div class='form-check m-3' onclick='checkedBox(this)'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
+        let form = list+"<input type='number' id='"+item+"Input' min='0' style='width:75px;' value='"+amount+"' placeholder='"+amount+"'>";
         list += amount+unit+" "+item; //how it displays: 5x bananas
-        form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable (not available yet)
+        form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable
         list += "</label></div>";
 
         finalList += list;
@@ -56,4 +56,60 @@ function load() {
     document.getElementById("remove").classList.add("hide");
     //document.getElementById("form").innerHTML = empty;
   }
+}
+
+function update() {
+  let storage = localStorage.getItem('storage'); //{1:['bananas',5,'x'],2:['flour',100,'mg']}
+  //alert(storage);
+  console.log(storage);
+
+  try {
+      const obj = JSON.parse(storage);
+
+      //console.log(obj['1'][1]+obj['1'][2]+" "+obj['1'][0]);
+      //5x bananas
+      let newList = "";
+      let newForm = "";
+
+      const keys = Object.keys(obj);
+      // print all keys
+      console.log(keys); //1:[banana,5,x]
+      // iterate over object
+      var shoppingList = "{";
+      let number = 0;
+
+      keys.forEach((key) => {
+        //console.log(`${key}: ${obj[0][0]}`);
+      //});
+
+        let amount = obj[key][1];
+        let unit = obj[key][2];
+        let item = obj[key][0];
+
+        let itemID = item+"Input";
+        let newAmount = document.getElementById(itemID).value;
+        if (newAmount!=0) {
+          let list = "<div class='form-check m-3' onclick='checkedBox(this)'><input class='form-check-input' type='checkbox' id='"+item+"Box'><label class='form-check-label' for='"+item+"Box'>"
+          let form = list+"<input type='number' id='"+item+"Input' style='width:75px;' min='0' value='"+newAmount+"' placeholder='"+newAmount+"'>";
+          list += newAmount+unit+" "+item; //how it displays: 5x bananas
+          form += unit+" "+item+"</label></div>" //for the form it is [5]x bananas where 5 is changeable
+          list += "</label></div>";
+
+          newList += list;
+          newForm += form;
+          number += 1;
+          shoppingList+=`"`+number+`":["${item}",${newAmount},"${unit}"],`;
+        }else {
+          //delete from list somehow
+        }
+      });
+        shoppingList = shoppingList.slice(0, -1); //removes comma
+        shoppingList +=`}`;
+
+        document.getElementById("shopping").innerHTML = newList;
+        document.getElementById("form").innerHTML = newForm;
+        localStorage.setItem('storage',shoppingList); //new list is saved
+    } catch {
+      //error
+    }
 }
