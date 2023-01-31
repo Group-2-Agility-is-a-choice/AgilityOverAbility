34c049ffc48d766e51a2da07c99769bd3168b6da
NiamhGedge <nfgedge@icloud.com>
2023-01-31 14:31:16 +0000

    improved code

--- LoginJava.js
+++ LoginJava.js
@@ -1,20 +1,18 @@
 function passLoginDeets(){
-    function showAllRecipes(){let username = "";
-            username += document.getElementById("username").innerHTML;
-            username += "&username";
-
-            let password = "";
-            password += document.getElementById("password").innerHTML;
-            password += "&password";
+    let username = "";
+    username += document.getElementById("user").innerHTML;
+    username += "&user";
+    let password = "";
+    password += document.getElementById("pass").innerHTML;
+    password += "&pass";
     fetch("backend/?loginDetails&user=Adam&pass=notAdam").then((rtn)=>{
         rtn.json().then((data)=>{
          setCookie(data.token);
          window.location.replace = "WHATEVER THE ADMIN SITE IS CALLED";
         })
-
     });
 }
-}
+
 
 function setCookie(value){
     const d = new Date();
@@ -23,19 +21,5 @@ function setCookie(value){
     document.cookie = "Jeffery =" + value + ";" + expires + ";path=/";
 }
 
-function getCookie() {
-    let name = "Jeffery =";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        window.location.replace = "WHATEVER THE ADMIN SITE IS CALLED";
-      }
-    }
-    return "";
-}
 
 
