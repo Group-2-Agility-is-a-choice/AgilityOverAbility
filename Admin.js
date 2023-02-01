e62cb6a012890641ad2d8e8e06918ebae0231731
NiamhGedge <nfgedge@icloud.com>
2023-02-01 11:31:39 +0000

    cookie

--- Admin.js
+++ Admin.js
@@ -1,5 +1,5 @@
 function showAllRecipes(){
-    fetch("backend/?searchRecipes").then((rtn)=>{
+    fetch("backend/?searchRecipes&key=" + getCookie('jack')).then((rtn)=>{
         rtn.json().then((data)=>{
             let adminRecipeList = "<table style='list-style:none;'><tr><th>Title</th><th>Edit</th><th>Delete</th></tr>";
             data.seachRecipes.foreach((item)=>{
@@ -30,4 +30,19 @@ function deleteButton(){
 function addRecipe(){
     let addRecipe = "";
     addRecipe += "&addRecipe=";
+}
+
+function getCookie(){
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+    if (c.indexOf(name) == 0) {
+        return;
+        }
+    }
+    return "SOME ERROR FROM BACKEND";
 }
