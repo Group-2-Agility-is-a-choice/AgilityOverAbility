d5a063f50d11dfaab2a49074424e246514c717e9
lujamitch@gmail.com <lujamitch@gmail.com>
2023-02-02 16:52:18 +0000

    basic pdf creation

--- backend/routes/createPDF.php
+++ backend/routes/createPDF.php
@@ -1,25 +1,20 @@
 <?php 
 
+require('fpdf185/fpdf.php'); 
 
+if (isset($_GET['RecipeID'])){
 
+    $pdf = new FPDF();
+    $pdf->AddPage(); 
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40,10,'Hello World!');
+    $pdf->Output();
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return [
+    "content-type" => "application/pdf",
+    "content" => json_encode($pdf, JSON_PRETTY_PRINT)
+];
 
 ?>
