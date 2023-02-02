9210776d9a0f233cd34358181fde0b8cec267fb6
nicolejackson2 <2415277@dundee.ac.uk>
2023-02-02 11:45:35 +0000

    login now working

--- LoginJava.js
+++ LoginJava.js
@@ -1,14 +1,12 @@
 function passLoginDeets(){
-    let username = "";
-    username += document.getElementById("user").innerHTML;
-    username += "&user";
-    let password = "";
-    password += document.getElementById("pass").innerHTML;
-    password += "&pass";
-    fetch("backend/?loginDetails&user=Adam&pass=notAdam").then((rtn)=>{
+    fetch("backend/?AdminLogin",{method:'POST',headers:{'Content-Type':"application/x-www-form-urlencoded; charset=UTF-8"},
+"body":Object.entries({
+    "username":document.getElementById("user").value,
+    "password":document.getElementById("pass").value
+}).map(([k,v])=>{return k+"="+v}).join('&')}).then((rtn)=>{
         rtn.json().then((data)=>{
          setCookie(data.token);
-         window.location.replace = "WHATEVER THE ADMIN SITE IS CALLED";
+         window.location.replace("admin.html");
         })
     });
 }
