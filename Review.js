803624f38a26d30fdcfa51f36c40f1cce3ef3bd6
NiamhGedge <nfgedge@icloud.com>
2023-02-02 11:53:35 +0000

    ill change it later

--- Review.js
+++ Review.js
@@ -1,23 +1,32 @@
 function allReviews(RecipeID){
-    fetch("backend/?searchRecipes&key=" + getCookie('jack')).then((rtn)=>{
+    fetch("backend/?getReviews").then((rtn)=>{
         rtn.json().then((data)=>{
             let reviewList = "<ul>";
             data.seachRecipes.foreach((item)=>{
                 reviewList += `<li>${item.Rating} ${item.Content} ${item.Image}</li>`;
             }); 
             reviewList += `</ul>`;  
-            document.getElementById("displayReviews").innerHTML = recipeList; 
+            document.getElementById("getReviews").innerHTML = recipeList; 
         })
 
     });
 }
 
 function createReview(){
-    let edit = "";
-    edit += document.getElementById("createReview").innerHTML;
-    edit += "&createReview=" + item.RecipeID;
+            let review = "";
+            review += document.getElementById("addReview").innerHTML;
+            review += "&addReview=" + item.RecipeID;
+            var starAmount = document.getElementById('reviewStars').innerHTML = `<b class="starsReview">${'⭐'.repeat(document.getElementById("stars").value)}</b><b>${'⭐'.repeat(5 - (document.getElementById("stars").value))}</b>`;
+            starAmount += "&addReview" + item.RecipeID;
+    fetch("backend/?addReview").then((rtn)=>{
+        rtn.json().then((data)=>{
+        })
+    })
 }
 
-function addReviewNumber(){
-    document.getElementById('reviewStars').innerHTML = `<b class="starsReview">${'â­'.repeat(document.getElementById("stars").value)}</b><b>${'â­'.repeat(5 - (document.getElementById("stars").value))}</b>`;
+function deleteReview(){
+    fetch("backend/?deleteReview&reviewDelete="+id).then((rtn)=>{
+        rtn.json().then((data)=>{
+        })
+    })
 }
