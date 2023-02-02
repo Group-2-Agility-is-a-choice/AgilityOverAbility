f221c7f7a647239581ee853495b34f3208d96ef9
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2023-02-02 10:31:26 +0000

    new ingredients working

--- Recipe.js
+++ Recipe.js
@@ -59,39 +59,44 @@ function updatePage(id)
 }
 
 function addList() {
-      if (localStorage.length != 0) {
-          let storage = localStorage.getItem('storage');
-          const obj = JSON.parse(storage);
-          const ingObj = JSON.parse(displayList); //updated ingredient list
-          const keys = Object.keys(obj);
-          const ingKeys = Object.keys(ingObj);
-          // print all keys
-          let tempList = "{";
-          let number = 0;
-          ingKeys.forEach((x)=>{
-            let listedAmount = ingObj[x][1];
-            let listedUnit = ingObj[x][2];
-            let listedItemName = ingObj[x][0];
-            keys.forEach((key)=>{
-              let amount = obj[key][1];
-              let unit = obj[key][2]; //not used but probs should implement
-              let itemName = obj[key][0];
+    if (localStorage.length != 0) {
+        let storage = localStorage.getItem('storage');
+        const obj = JSON.parse(storage);
+        const ingObj = JSON.parse(displayList); //updated ingredient list
+        const keys = Object.keys(ingObj);
+        const loopKey = Object.keys(obj);
+        // print all keys
+        let number = Object.keys(obj).length;//get end of list?
+        keys.forEach((key)=>{
+          let amount = ingObj[key][1];
+          let unit = ingObj[key][2]; //not used but probs should implement
+          let itemName = ingObj[key][0];
 
-              if (itemName==listedItemName) {
-                //in list so add to new list
-                number += 1;
-                tempList+=`"`+number+`":["${itemName}",${amount+listedAmount},"${unit}"],`;
-              }
-            })
-          }) //so far only items that are the same are added
+          //tempList+=`"`+number+`":["${itemName}",${amount+listedAmount},"${unit}"],`;
 
+          let index = -1;
+          loopKey.forEach((loop)=>{ //look if current item is in basket
+            if (ingObj[key][0]==obj[loop][0]) {
+              index = loop; //position in object array
+            }
+          })
 
-    tempList = tempList.slice(0, -1);
-    tempList +=`}`;
+          if (index!=-1) { //if found
+            //if object in list,
+            obj[index][1] += amount; //adds new info
+          } else {
+            //not in list, so add to end
+            number += 1;
+            obj[number] = [itemName,amount,unit];
+          }
+        }) //so far only items that are the same are added
+    alert("Items added to cart.");//change to modal display
+    tempList = JSON.stringify(obj); //need to throw in a try catch?
     localStorage.setItem('storage', tempList);
     console.log("Temp "+tempList);
   } else {
       localStorage.setItem('storage', displayList); //list is empty so add just this
+      alert("Items added to cart.");
       console.log("Display "+displayList);
   }
 }
