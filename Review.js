8cbe9d83f1fdf00bb8ca3d214dcc70716277dd1c
nicolejackson2 <2415277@dundee.ac.uk>
2023-02-02 16:50:23 +0000

    admin reviews now working

--- Review.js
+++ Review.js
@@ -1,4 +1,4 @@
-function allReviews(RecipeID){
+function allReviews(){
     fetch("backend/?getReviews").then((rtn)=>{
         rtn.json().then((data)=>{
             let reviewList = "";
@@ -12,6 +12,7 @@ function allReviews(RecipeID){
                     <h4 class="card-title">${item.Title}</h4>
                     <p class="card-text mt-4"><small class="text-muted">Rating: ${item.Rating}</small></p>
                     <p class="card-text"><small class="text-muted">${item.Content}</small></p>
+                    <button id="myBtn" onclick="deleteButton(this.getAttribute('data-id'))"  data-id = "${item.ReviewID}">Delete </button> 
               </div>
             </div>
           </div></div>`;
@@ -20,6 +21,12 @@ function allReviews(RecipeID){
     })
 
 });
+}
+
+function deleteButton(id){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    modal.setAttribute("data-id", id);
 }
 
 function createReview(){
@@ -35,8 +42,11 @@ function createReview(){
 }
 
 function deleteReview(){
-    fetch("backend/?deleteReview&reviewDelete="+id).then((rtn)=>{
+    let id = document.getElementById("myModal").getAttribute('data-id')
+    fetch("backend/?deleteReview&ReviewID="+id).then((rtn)=>{
         rtn.json().then((data)=>{
+            let id = document.getElementById("myModal").style.display = 'none';
+            allReviews();
         })
     })
 }
